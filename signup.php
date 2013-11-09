f394f6360b87a2b22f7752c401c02615b38e2be5
douug <dgbx60@gmail.com>
2013-11-09 15:24:10 -0800

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,14 @@
+<p>Sign up for Shejul!</p>
+
+<p>Please choose your</p>
+<p><font size="2"> Login and&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+password</font></p>
+
+<form method="POST" action="oracle-test.php">
+
+<!--refresh page when submit-->
+
+<p><input type="text" name="insNo" size="6">
+<input type="text" name="insName" 
+size="18"></p>
+</form>
