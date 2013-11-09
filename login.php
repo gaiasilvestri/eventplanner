a5b291537c7f14c8367013bf2e4a613ae33747d8
douug <dgbx60@gmail.com>
2013-11-09 15:21:03 -0800

    login page

---
+++ login.php
@@ -0,0 +1,11 @@
+<p>Sign in to Shejul.</p>
+
+<p>Please enter your login:</p>
+<p><font size="2"> Login&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Password</font></p>
+
+<p><input type="text" name="insNo" size="6">
+<input type="text" name="insName" 
+size="18"></p>
+
+<p>Or sign up for Shejul <a href="http://www.ugrad.cs.ubc.ca/~j8d7/304signup.php/">here!</a> </p>
+</form>
