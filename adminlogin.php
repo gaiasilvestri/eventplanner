4dbc2605447f783872cddc2921456140abecb101
danepet <danecpeterson@gmail.com>
2013-11-09 15:38:39 -0800

    Create adminlogin.php

---
+++ adminlogin.php
@@ -0,0 +1,13 @@
+<DOCTYPE html>
+	<html>
+	<body>
+	
+
+	<p>Sign in to Shejul.</p>
+
+	<p>Please enter your login:</p>
+	<p><font size="2"> Admin ID&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Password</font></p>
+
+	<p><input type="text" name="insadminNo" size="6">
+	<input type="text" name="insadminName" size="18"></p>
+</html>
