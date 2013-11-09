ebd25790c0e884aacfba23e509c9ec408a7ab1f0
danepet <danecpeterson@gmail.com>
2013-11-09 15:37:46 -0800

    Added Admin sign-in link

--- login.php
+++ login.php
@@ -1,14 +1,23 @@
-<p>Sign in to Shejul.</p>
+<DOCTYPE html>
+	<html>
+	<body>
+	
 
-<p>Please enter your login:</p>
-<p><font size="2"> Login&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Password</font></p>
+	<p>Sign in to Shejul.</p>
 
-<p><input type="text" name="insNo" size="6">
-<input type="text" name="insName" 
-size="18"></p>
+	<p>Please enter your login:</p>
+	<p><font size="2"> Login&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Password</font></p>
 
-<p>Or sign up for Shejul <a href="http://www.ugrad.cs.ubc.ca/~j8d7/304signup.php/">here!</a> </p>
-</form>
+	<p><input type="text" name="insNo" size="6">
+	<input type="text" name="insName" 
+			size="18"></p>
+
+	<p>Or sign up for Shejul <a href="http://www.ugrad.cs.ubc.ca/~j8d7/304signup.php/">here!</a> </p>
+	<br>
+	<br>
+		<p>Admin sign-in <a href="http://www.ugrad.cs.ubc.ca/~l9e8/adminlogin.php/">here!</a> </p>
+		</body>
+</html>
 
 <!--TODO
     write PHP to interface w/ Users table-->
